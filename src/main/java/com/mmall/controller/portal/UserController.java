18e1006b334b5fa54d80d193fd2e1b0aeacfcd8a
隆曦 <xulongxi@foxmail.com>
2018-01-28 12:35:51 +0800

    加了Spring MVC全局异常处理

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -45,6 +45,7 @@ public class UserController {
     @RequestMapping(value = "login.do", method = RequestMethod.POST)
     @ResponseBody
     public ServerResponse<User> login(String username, String password, HttpSession session, HttpServletResponse httpServletResponse) {
+        int j = 6/0;
         ServerResponse<User> response = iUserService.login(username, password);
         if (response.isSuccess()) {
 //            session.setAttribute(Const.CURRENT_USER, response.getData());
